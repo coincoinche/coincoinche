ca427659e5561548dcf2b2b2d3098f98a9b2a891
Corentin Carteau <corentin.carteau.intern@birdie.care>
2019-09-20 17:17:07 +0200

    run tests in travis.yml

--- src/main/java/com/coincoinche/HelloController.java
+++ src/main/java/com/coincoinche/HelloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @RequestMapping("/")
-  public String index() {
+  @RequestMapping("/greetings")
+  public String greet() {
     return "Greetings from Spring Boot!";
   }
 }
--- src/test/java/com/coincoinche/CoincoincheApplicationTests.java
+++ src/test/java/com/coincoinche/CoincoincheApplicationTests.java
@@ -2,13 +2,27 @@ package com.coincoinche;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CoincoincheApplicationTests {
 
+  @Autowired
+  private HelloController controller;
+
   @Test
-  public void contextLoads() {}
+  public void contextLoads() {
+    assertThat(controller).isNotNull();
+  }
+
+  @Test
+  public void greetsUser() {
+    String result = controller.greet();
+    assertThat(result).isEqualTo("Greetings from Spring Boot!");
+  }
 }
