c9e9137faa121633daef6c70bd9ea0ad1de98ab6
Corentin Carteau <corentin.carteau.intern@birdie.care>
2019-09-20 20:54:12 +0200

    add formatted files before commiting...

--- src/test/java/com/coincoinche/CoincoincheApplicationTests.java
+++ src/test/java/com/coincoinche/CoincoincheApplicationTests.java
@@ -1,19 +1,18 @@
 package com.coincoinche;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CoincoincheApplicationTests {
 
-  @Autowired
-  private HelloController controller;
+  @Autowired private HelloController controller;
 
   @Test
   public void contextLoads() {
