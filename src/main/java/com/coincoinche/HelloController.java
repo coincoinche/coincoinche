77f00a1e8796a6b36ea4fcefb776e9a8cf8888c1
nockty <corentin.dupret@student.ecp.fr>
2019-09-27 11:17:28 +0200

    :loud_sound: Add log examples in HelloController

--- src/main/java/com/coincoinche/HelloController.java
+++ src/main/java/com/coincoinche/HelloController.java
@@ -1,5 +1,7 @@
 package com.coincoinche;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+  private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+  /** Test endpoint for greetings. */
   @RequestMapping("/greetings")
   public String greet() {
+    logger.trace("this is a TRACE log");
+    logger.debug("this is a DEBUG log");
+    logger.info("this is a INFO log");
+    logger.warn("this is a WARN log");
+    logger.error("this is a ERROR log");
     return "Greetings from Spring Boot!";
   }
 }
