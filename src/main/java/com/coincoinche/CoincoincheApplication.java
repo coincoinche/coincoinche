6fef631d5aa9356796e7133f48a18934dfa16346
Corentin Carteau <corentin.carteau.intern@birdie.care>
2019-09-29 17:21:09 +0200

    set-up-cors

--- src/main/java/com/coincoinche/CoincoincheApplication.java
+++ src/main/java/com/coincoinche/CoincoincheApplication.java
@@ -2,9 +2,22 @@ package com.coincoinche;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CoincoincheApplication {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/greetings").allowedOrigins("http://localhost:3000");
+      }
+    };
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(CoincoincheApplication.class, args);
--- src/main/java/com/coincoinche/HelloController.java
+++ src/main/java/com/coincoinche/HelloController.java
@@ -2,7 +2,7 @@ package com.coincoinche;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +14,7 @@ public class HelloController {
   private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
   /** Test endpoint for greetings. */
-  @RequestMapping("/greetings")
+  @GetMapping("/greetings")
   public String greet() {
     logger.debug("this is a DEBUG log");
     logger.info("this is a INFO log");
