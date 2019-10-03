38755a750b60f4c246928629731f994fa9ddd207
Corentin Carteau <corentin.carteau.intern@birdie.care>
2019-10-03 16:04:37 +0200

    enable-player-to-play-cards

--- web-ui/src/pages/MainGame/MainGameScreen.tsx
+++ web-ui/src/pages/MainGame/MainGameScreen.tsx
@@ -6,41 +6,104 @@ import CardBoard from "../../components/cards/CardBoard";
 import {GameState, Position} from "./types";
 
 const makeOtherPlayer = () => ({
-  authorisedPlays: [],
-  cardsInHand: new Array(8).fill(CardValue.blue_back),
+  authorisedPlays: [CardValue.blue_back],
+  cardsInHand: new Array(4).fill(CardValue.blue_back),
 });
 
 const players = {
-  top: makeOtherPlayer(),
-  right: makeOtherPlayer(),
-  left: makeOtherPlayer(),
-  bottom: {
-    authorisedPlays: [],
-    cardsInHand: [CardValue.ac, CardValue.ad, CardValue.ah, CardValue.as, CardValue.jc, CardValue.jd, CardValue.jh, CardValue.js],
+  [Position.top]: makeOtherPlayer(),
+  [Position.right]: makeOtherPlayer(),
+  [Position.left]: makeOtherPlayer(),
+  [Position.bottom]: {
+    authorisedPlays: [CardValue.ac, CardValue.ad, CardValue.ah, CardValue.as, CardValue.jc, CardValue.jd, CardValue.jh, CardValue.js],
+    cardsInHand: [CardValue.jc, CardValue.jd, CardValue.jh, CardValue.js],
   }
+};
+
+const getNextPlayer = (currentPlayer: Position): Position => {
+  if (currentPlayer === Position.bottom) {
+    return Position.left;
+  }
+
+  if (currentPlayer === Position.left) {
+    return Position.top;
+  }
+
+  if (currentPlayer === Position.top) {
+    return Position.right;
+  }
+
+  if (currentPlayer === Position.right) {
+    return Position.bottom;
+  }
+  throw new Error('Unknown position');
 };
 
 export default class MainGameScreen extends React.Component<{}, GameState> {
   state = {
     players,
     currentPlayer: Position.bottom,
-    currentTrick: { topCard: undefined, leftCard: undefined, rightCard: undefined, bottomCard: undefined },
+    currentTrick: { top: undefined, left: undefined, right: undefined, bottom: undefined },
+  };
+
+  onCardPlayed = (player: Position, card: CardValue) => {
+    if (
+        this.state.players[player].cardsInHand.indexOf(card) === -1 ||
+        this.state.players[player].authorisedPlays.indexOf(card) === -1 ||
+        player !== this.state.currentPlayer
+    ) {
+      return;
+    }
+
+    this.setState(prevState => ({
+      currentPlayer: getNextPlayer(prevState.currentPlayer),
+      currentTrick: {
+        ...this.state.currentTrick,
+        [prevState.currentPlayer]: card,
+      },
+      players: {
+        ...prevState.players,
+        [prevState.currentPlayer]: {
+          authorisedPlays: [],
+          cardsInHand: prevState.players[prevState.currentPlayer].cardsInHand.filter(cardInHand => cardInHand !== card),
+        }
+      }
+    }))
   };
 
   render() {
     return <Container direction="column">
-      <HandOfCards cards={this.state.players.top.cardsInHand} rotationDegrees={180} scale={0.8} />
+      <HandOfCards
+          cards={this.state.players[Position.top].cardsInHand}
+          rotationDegrees={180}
+          scale={0.8}
+          onCardPlayed={(card: CardValue) => this.onCardPlayed(Position.top, card)}
+      />
       <Container direction="row" justifyContent="space-between">
-        <HandOfCards cards={this.state.players.left.cardsInHand} rotationDegrees={90}  scale={0.8} />
-        <CardBoard
-          leftCard={this.state.currentTrick.leftCard}
-          rightCard={this.state.currentTrick.rightCard}
-          bottomCard={this.state.currentTrick.bottomCard}
-          topCard={this.state.currentTrick.topCard}
+        <HandOfCards
+          cards={this.state.players[Position.left].cardsInHand}
+          rotationDegrees={90}
+          scale={0.8}
+          onCardPlayed={(card: CardValue) => this.onCardPlayed(Position.left, card)}
         />
-        <HandOfCards cards={this.state.players.right.cardsInHand} rotationDegrees={-90} scale={0.8} />
+        <CardBoard
+          left={this.state.currentTrick[Position.left]}
+          right={this.state.currentTrick[Position.right]}
+          bottom={this.state.currentTrick[Position.bottom]}
+          top={this.state.currentTrick[Position.top]}
+        />
+        <HandOfCards
+          cards={this.state.players[Position.right].cardsInHand}
+          rotationDegrees={-90}
+          scale={0.8}
+          onCardPlayed={(card: CardValue) => this.onCardPlayed(Position.right, card)}
+        />
       </Container>
-      <HandOfCards cards={this.state.players.bottom.cardsInHand} rotationDegrees={0} />
+      <HandOfCards
+        cards={this.state.players[Position.bottom].cardsInHand}
+        rotationDegrees={0}
+        onCardPlayed={(card: CardValue) => this.onCardPlayed(Position.bottom, card)}
+      />
     </Container>;
   }
 }
