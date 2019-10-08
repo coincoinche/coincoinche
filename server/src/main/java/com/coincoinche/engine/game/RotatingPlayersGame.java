abf71d0885884a1c0060ea729aed10cfed9e8fbb
nockty <corentin.dupret@student.ecp.fr>
2019-10-08 15:34:46 +0200

    :alembic: Add rotating players game abstraction
    Add helper classes for a rotating game of two teams. It is
    useful because it can be used both for the coinche global
    game and the coinche game round.

---
+++ server/src/main/java/com/coincoinche/engine/game/RotatingPlayersGame.java
@@ -0,0 +1,36 @@
+package com.coincoinche.engine.game;
+
+import java.util.List;
+
+/** Implement a game with rotating players. */
+public class RotatingPlayersGame<P> {
+
+  private List<P> players;
+  private int currentPlayerIndex = 0;
+
+  public void setPlayers(List<P> players) {
+    this.players = players;
+  }
+
+  /**
+   * Get the player who should play currently.
+   *
+   * @return currrent player.
+   */
+  protected P getCurrentPlayer() {
+    return players.get(currentPlayerIndex);
+  }
+
+  /** Rotate players turn. */
+  protected void rotatePlayers() {
+    currentPlayerIndex = currentPlayerIndex + 1 % players.size();
+  }
+
+  public int getCurrentPlayerIndex() {
+    return currentPlayerIndex;
+  }
+
+  public void setCurrentPlayerIndex(int currentPlayerIndex) {
+    this.currentPlayerIndex = currentPlayerIndex;
+  }
+}
