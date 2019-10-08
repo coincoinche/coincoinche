7286547d2bed50418cad044e4e4a9fba30088e71
nockty <corentin.dupret@student.ecp.fr>
2019-10-08 16:30:46 +0200

    :construction: Improve game state interface
    Add a GameStateTerminal interface as well for states
    that are final.

--- server/src/main/java/com/coincoinche/engine/GameStateInterface.java
+++
@@ -1,9 +0,0 @@
-package com.coincoinche.engine;
-
-import java.util.List;
-
-/** Interface representing the state of a game phase. */
-public interface GameStateInterface {
-
-  List<MoveInterface> getLegalMoves();
-}
