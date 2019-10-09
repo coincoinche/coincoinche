ae9245993422b233efd46332aa133db26332d1aa
Corentin Carteau <corentin.carteau.intern@birdie.care>
2019-10-09 23:36:51 +0200

    adapt front end

--- web-ui/src/websocket/socket-messages.ts
+++ web-ui/src/websocket/socket-messages.ts
@@ -1,5 +1,6 @@
 import {MESSAGE_TYPE, SocketMessage} from "./types";
 
-export const joinLobbyMessage: SocketMessage = {
+export const makeJoinLobbyMessage = (username: string) => ({
   type: MESSAGE_TYPE.JOIN_LOBBY,
-};
+  username
+});
