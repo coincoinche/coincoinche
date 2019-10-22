a0e405c214e4fd5051d0f4c06cf244381a8df85e
Cédric Talbot <cedric.talbot@student.ecp.fr>
2019-10-22 15:56:32 +0200

    Add standalone ladder page

--- web-ui/src/pages/Ladder/Ladder.tsx
+++ web-ui/src/pages/Ladder/Ladder.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { Component, FormEvent } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import './Ladder.css';
 
-export default class Ladder extends React.Component {
+type LadderState = {
+  searchedUser : string;
+}
+
+export default class Ladder extends Component<{}, LadderState> {
+  constructor(props : any) {
+    super(props);
+    this.state = {searchedUser: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: FormEvent<HTMLInputElement>) {
+    this.setState({searchedUser: event.currentTarget.value});
+  }
+
   render() {
-    return <div>
-      Ladder screen
-    </div>;
+    const data = [{
+      username: 'Tanner Linsley',
+      score: 26,
+      rank: 1
+    }]
+    const filteredData = data.filter(u => u.username.toLowerCase().includes(this.state.searchedUser));
+    const columns = [{
+      Header: (props : any) => <span className='columnName'>Rank</span>,
+      accessor : 'rank',
+      Cell: (props : any) => <span className='columnItem'>{props.value}</span>
+    }, {
+      Header: (props : any) => <span className='columnName'>User</span>,
+      accessor: 'username',
+      Cell: (props : any) => <span className='columnItem'>{props.value}</span>
+    }, {
+      Header: (props : any) => <span className='columnName'>Score</span>,
+      accessor: 'score',
+      Cell: (props : any) => <span className='columnItem'>{props.value}</span>
+    }]
+
+    return (
+      <div>
+        <div className='title'>
+          User ladder
+        </div>
+        <div className='container'>
+          <div className='table'>
+            <ReactTable
+              data={filteredData}
+              columns={columns}
+            />
+          </div>
+          <div className='research'>
+            <label>Search User</label>
+            <input className='researchInput' onChange={this.handleChange}/>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
