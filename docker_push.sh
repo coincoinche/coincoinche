35e4578f30c5ab066d52108ff4a2ec0de88f781a
nockty <corentin.dupret@student.ecp.fr>
2019-10-31 15:06:03 +0100

    :lock: Set pass to store Docker credentials in CI

--- docker_push.sh
+++ docker_push.sh
@@ -3,7 +3,40 @@
 IMAGE_FRONT="${REGISTRY_URL}/${DOCKER_REPO_FRONT}"
 IMAGE_BACK="${REGISTRY_URL}/${DOCKER_REPO_BACK}"
 
+set -e
+
+# init key for pass to avoid storing unencrypted password
+export GPG_TTY=$(tty)
+gpg2 --batch --gen-key <<-EOF
+%no-protection
+%echo Generating a standard key
+Key-Type: 1
+Key-Length: 2048
+Subkey-Type: 1
+Subkey-Length: 2048
+Name-Real: coincoinche [travis-ci]
+Name-Email: coincoinche@travis-ci
+Expire-Date: 0
+# Do a commit here, so that we can later print "done" :-)
+%commit
+%echo done
+EOF
+
+key=$(gpg2 --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
+pass init $key
+
+# set helper to store docker credentials with pass
+curl -fsSL https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz | tar -xz
+export PATH=$PATH:$(pwd)
+chmod +x docker-credential-pass
+mkdir ~/.docker
+touch ~/.docker/config.json
+echo '{"auths": {}, "credsStore": "pass"}' >> ~/.docker/config.json
+
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin ${REGISTRY_URL}
+sed -ie "s/$REGISTRY_URL/https:\/\/$REGISTRY_URL/g" ~/.docker/config.json
 
 docker push ${IMAGE_FRONT}
 docker push ${IMAGE_BACK}
+
+docker logout ${REGISTRY_URL}
