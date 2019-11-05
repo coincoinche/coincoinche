f1a7b3d80f114caad1df95c4c4df0e094dadeed8
nockty <corentin.dupret@student.ecp.fr>
2019-11-05 11:16:55 +0100

    :green_heart: Fix Docker build

--- docker_build.sh
+++ docker_build.sh
@@ -4,7 +4,7 @@ IMAGE_FRONT="${REGISTRY_URL}/${DOCKER_REPO_FRONT}"
 IMAGE_BACK="${REGISTRY_URL}/${DOCKER_REPO_BACK}"
 
 cd ./web-ui
-docker build -t ${IMAGE_BACK} --build-arg ENVIRONMENT=${BUILD_ENVIRONMENT} .
-cd ../server
 docker build -t ${IMAGE_FRONT} --build-arg REACT_APP_API_BASE_URL=${BUILD_API_URL} --build-arg BACKEND_NAME=${BUILD_BACKEND} .
+cd ../server
+docker build -t ${IMAGE_BACK} --build-arg ENVIRONMENT=${BUILD_ENVIRONMENT} .
 cd ..
