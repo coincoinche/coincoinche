f7f6461e3301777b86f97fd5ba184416e352e6d7
nockty <corentin.dupret@student.ecp.fr>
2019-11-19 13:13:12 +0100

    :fire: Remove events

--- server/src/main/java/com/coincoinche/events/BiddingTurnStartedEvent.java
+++
@@ -1,7 +0,0 @@
-package com.coincoinche.events;
-
-public class BiddingTurnStartedEvent extends TurnStartedEvent {
-  public BiddingTurnStartedEvent(String[] legalMoves, int playerIndex) {
-    super(EventType.BIDDING_TURN_STARTED, legalMoves, playerIndex);
-  }
-}
