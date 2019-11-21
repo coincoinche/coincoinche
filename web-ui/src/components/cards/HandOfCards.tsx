9d61ddf177c1b7ec04f93c555b474426116eaf08
nockty <corentin.dupret@student.ecp.fr>
2019-11-21 10:13:11 +0100

    :lipstick: Add username, rating, and current player info to UI

--- web-ui/src/components/cards/HandOfCards.tsx
+++ web-ui/src/components/cards/HandOfCards.tsx
@@ -9,28 +9,67 @@ type Props = {
   scale?: number;
   onCardPlayed: (card: CardValue) => void;
   cardsBorderHighlight?: boolean[];
+  playerName: string;
+  playerRating: number;
+  currentPlayer: boolean;
 }
 
 type ContainerProps = {
   rotationDegrees: number;
 }
 
+type PlayerRatingProps = {
+  rotationDegrees: number;
+  currentPlayer: boolean;
+}
+
+const ColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  transform: ${({rotationDegrees}: ContainerProps) => `rotate(${rotationDegrees}deg)`};
+`
+
+const PlayerRating = styled.div`
+  margin: 15px;
+  background-color: #CC3329;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-style: solid;
+  border-radius: 20px;
+  transform: ${({rotationDegrees}: PlayerRatingProps) => `rotate(${rotationDegrees}deg)`};
+  ${
+    ({currentPlayer}: PlayerRatingProps) => currentPlayer && `
+      box-shadow:
+        0 0 30px 15px rgb(255, 255, 255, 0.5),
+        0 0 50px 30px rgb(255, 254, 173, 0.7),
+        0 0 70px 45px rgb(255, 254, 173, 0.9);
+    `
+  }
+`
+
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  transform: ${({rotationDegrees}: ContainerProps) => `rotate(${rotationDegrees}deg)`};
 `;
 
 const ROTATION_MAX_DEGREE_ANGLE = 20;
 
-const HandOfCards = ({cards, rotationDegrees, scale, onCardPlayed, cardsBorderHighlight}: Props) => {
+const HandOfCards = ({cards, rotationDegrees, scale, onCardPlayed, cardsBorderHighlight, playerName, playerRating, currentPlayer}: Props) => {
   const rotationStep = cards.length > 1 ? 2 * ROTATION_MAX_DEGREE_ANGLE / (cards.length - 1) : 0;
   const yTranslationMax = 15 * cards.length;
   const yTranslationStep = 2 * yTranslationMax / (cards.length - 1);
   return (
-      <Container rotationDegrees={rotationDegrees}>
+      <ColumnContainer rotationDegrees={rotationDegrees}>
+      <PlayerRating rotationDegrees={-rotationDegrees} currentPlayer={currentPlayer}>
+          <b>{playerName}</b>
+          <br/>{playerRating}
+      </PlayerRating>
+      <Container>
         {
           cards.map((card, index) => {
             return (
@@ -50,6 +89,7 @@ const HandOfCards = ({cards, rotationDegrees, scale, onCardPlayed, cardsBorderHi
           })
         }
       </Container>
+      </ColumnContainer>
   );
 };
 
