47d276183d5388dec6f30a831ee777bb329426f2
Corentin Carteau <corentin.carteau.intern@birdie.care>
2019-11-22 17:01:27 +0100

    add animations and props

--- web-ui/src/components/misc/Title.tsx
+++ web-ui/src/components/misc/Title.tsx
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
+type Props = {
+  fontSize: number;
+}
+
 export default styled.div`
   color: #FFE136;
-  text-shadow: -7px 0 black, 0 7px black, 7px 0 black, 0 -7px black;
+  text-shadow: ${({ fontSize }: Props) => {
+    const shaddow = Math.round(fontSize / 20);
+    return `-${shaddow}px 0 black, 0 ${shaddow}px black, ${shaddow}px 0 black, 0 -${shaddow}px black`;
+  }};
   font-family: 'Cookie',cursive;
-  font-size: 140pt;
+  font-size: ${({ fontSize }: Props) => fontSize}pt;
   padding-bottom: 20px;
 `;
--- web-ui/src/pages/MainGame/EndGameScreen.tsx
+++ web-ui/src/pages/MainGame/EndGameScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Title from "../../components/misc/Title";
 import styled from "styled-components";
+import Title from "../../components/misc/Title";
+import {Animated} from "react-animated-css";
 import Container from "../../components/utils/Container";
 import Button from "../../components/misc/Button";
 
@@ -9,42 +10,170 @@ const TeamTitle = styled.h2`
   font-size: 2em;
 `;
 
-const TeamContainer = styled.div`
-  display: block;
-  text-align: left;
-  min-width: 30em;
+const ScoreContainer = styled.div`
+  background-color: #1F1F1F;
+  border-radius: 30px;
+  padding: 10px 30px;
+  border: 20px solid #8e5723;
 `;
 
 const Text = styled.p`
   color: #c9c9c9
 `;
 
-export default () => {
+const animationParams = {
+  title: {
+    type: "bounceInUp" as any,
+    duration: 1500,
+    delay: 0,
+  },
+  scoreBoard: {
+    type: "fadeIn" as any,
+    duration: 500,
+    delay: 1500,
+  },
+  yourTeamTitle: {
+    type: "bounceInLeft" as any,
+    duration: 1000,
+    delay: 2000,
+  },
+  yourTeamPlayers: {
+    type: "fadeIn" as any,
+    duration: 1000,
+    delay: 3000,
+  },
+  theirTeamTitle: {
+    type: "bounceInRight" as any,
+    duration: 1000,
+    delay: 2000,
+  },
+  theirTeamPlayers: {
+    type: "fadeIn" as any,
+    duration: 1000,
+    delay: 3000,
+  },
+  exitButton: {
+    type: "fadeIn" as any,
+    duration: 500,
+    delay: 4000,
+  }
+};
+
+type Props = {
+  win: boolean;
+  yourTeam: {
+    points: number;
+    players: {
+      username: string;
+      previousElo: number;
+      newElo: number;
+    }[];
+  };
+  theirTeam: {
+    points: number;
+    players: {
+      username: string;
+      previousElo: number;
+      newElo: number;
+    }[];
+  }
+}
+
+export default ({ win, yourTeam, theirTeam }: Props) => {
+  const { title, yourTeamTitle, yourTeamPlayers, theirTeamPlayers, theirTeamTitle, exitButton, scoreBoard } = animationParams;
   return (
     <Container direction="column">
-      <Title>Gagné!</Title>
-      <TeamTitle>Ton équipe</TeamTitle>
-      <Container direction="column">
-        <Container direction="row" justifyContent="space-between" minWidth="20em">
-          <Text>Username1</Text>
-          <Text>some score</Text>
-        </Container>
+      <Animated
+        animationIn={title.type}
+        animationInDelay={title.delay}
+        animationInDuration={title.duration}
+        animationOut="fadeOut"
+        isVisible={true}
+      >
+        {
+          win ? <Title fontSize={70}>Gagné!</Title> : <Title fontSize={70}>Perdu...</Title>
+        }
+      </Animated>
 
-        <Container direction="row" justifyContent="space-between" minWidth="20em">
-          <Text>Username2</Text>
-          <Text>some score</Text>
-        </Container>
-      </Container>
-      <TeamTitle>Équipe adverse</TeamTitle>
-      <Container direction="row" justifyContent="space-between" minWidth="20em">
-        <Text>Username3</Text>
-        <Text>some score</Text>
-      </Container>
-      <Container direction="row" justifyContent="space-between" minWidth="20em">
-        <Text>Username4</Text>
-        <Text>some score</Text>
-      </Container>
-      <Button>Retour au menu</Button>
+      <Animated
+        animationIn={scoreBoard.type}
+        animationInDelay={scoreBoard.delay}
+        animationInDuration={scoreBoard.duration}
+        animationOut="fadeOut"
+        isVisible={true}
+      >
+        <ScoreContainer>
+          <Animated
+            animationIn={yourTeamTitle.type}
+            animationInDelay={yourTeamTitle.delay}
+            animationInDuration={yourTeamTitle.duration}
+            animationOut="fadeOut"
+            isVisible={true}
+          >
+            <Container direction="row" justifyContent="space-between" minWidth="20em">
+              <TeamTitle>Ton équipe</TeamTitle>
+              <Text>{yourTeam.points}</Text>
+            </Container>
+          </Animated>
+
+          <Animated
+            animationIn={yourTeamPlayers.type}
+            animationInDelay={yourTeamPlayers.delay}
+            animationInDuration={yourTeamPlayers.duration}
+            animationOut="fadeOut"
+            isVisible={true}
+          >
+            <Container direction="column">
+              {
+                yourTeam.players.map(({ username, previousElo, newElo }) => (
+                  <Container direction="row" justifyContent="space-between" minWidth="20em">
+                    <Text>{username}</Text>
+                    <Text>{previousElo} -> {newElo}</Text>
+                  </Container>
+              ))}
+            </Container>
+          </Animated>
+
+          <Animated
+            animationIn={theirTeamTitle.type}
+            animationInDelay={theirTeamTitle.delay}
+            animationInDuration={theirTeamTitle.duration}
+            animationOut="fadeOut"
+            isVisible={true}
+          >
+            <Container direction="row" justifyContent="space-between" minWidth="20em">
+              <TeamTitle>Équipe adverse</TeamTitle>
+              <Text>{theirTeam.points}</Text>
+            </Container>
+          </Animated>
+
+          <Animated
+            animationIn={theirTeamPlayers.type}
+            animationInDelay={theirTeamPlayers.delay}
+            animationInDuration={theirTeamPlayers.duration}
+            animationOut="fadeOut"
+            isVisible={true}
+          >
+            {
+              theirTeam.players.map(({ username, previousElo, newElo }) => (
+                <Container direction="row" justifyContent="space-between" minWidth="20em">
+                  <Text>{username}</Text>
+                  <Text>{previousElo} -> {newElo}</Text>
+                </Container>
+            ))}
+          </Animated>
+        </ScoreContainer>
+      </Animated>
+
+      <Animated
+        animationIn={exitButton.type}
+        animationInDelay={exitButton.delay}
+        animationInDuration={exitButton.duration}
+        animationOut="fadeOut"
+        isVisible={true}
+      >
+        <Button>Retour au menu</Button>
+      </Animated>
     </Container>
   )
 }
