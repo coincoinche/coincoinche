186a847fff04841d3c6c2935140bcde123459f59
nockty <corentin.dupret@student.ecp.fr>
2019-11-26 14:25:05 +0100

    :bulb: Add javadoc to signup and login events

--- server/src/main/java/com/coincoinche/websockets/events/LogInEvent.java
+++ server/src/main/java/com/coincoinche/websockets/events/LogInEvent.java
@@ -1,5 +1,6 @@
 package com.coincoinche.websockets.events;
 
+/** Event received when a user logs in. */
 public class LogInEvent extends Event {
   private String username;
   private String password;
@@ -8,6 +9,12 @@ public class LogInEvent extends Event {
     super(EventType.LOG_IN);
   }
 
+  /**
+   * Create a new log in event with a username and password.
+   *
+   * @param username is the username of the user.
+   * @param password is their password.
+   */
   public LogInEvent(String username, String password) {
     super(EventType.LOG_IN);
     this.username = username;
--- server/src/main/java/com/coincoinche/websockets/events/SignUpEvent.java
+++ server/src/main/java/com/coincoinche/websockets/events/SignUpEvent.java
@@ -1,5 +1,6 @@
 package com.coincoinche.websockets.events;
 
+/** Event received when the user signs up. */
 public class SignUpEvent extends Event {
   private String username;
   private String password;
@@ -8,6 +9,12 @@ public class SignUpEvent extends Event {
     super(EventType.SIGN_UP);
   }
 
+  /**
+   * Create a new sign-up event for a user.
+   *
+   * @param username is the username of the user.
+   * @param password is their password.
+   */
   public SignUpEvent(String username, String password) {
     super(EventType.SIGN_UP);
     this.username = username;
